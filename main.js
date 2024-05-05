a019d62bcba1c0eabcb1cd1200662ba3ac5fb6cf
Niba-khan <164494729+Niba-khan@users.noreply.github.com>
2024-05-05 12:18:01 +0500

    "This is a CLI Based Marksheet"

---
+++ main.js
@@ -0,0 +1,94 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold.underline("\n\tMarkSheet For class Ten\n"));
+//Asking for roll Number:
+let studentInformation = await inquirer.prompt([{
+        name: "studentName",
+        type: "input",
+        message: chalk.red.italic("Enter your name")
+    },
+    {
+        name: "fatherName",
+        type: "input",
+        message: chalk.yellow.italic("Enter your father Name")
+    },
+]);
+let roll = await inquirer.prompt([
+    {
+        name: "number1",
+        type: "input",
+        message: chalk.bold.green.underline("Please Enter Your Roll Number and check your Marksheet")
+    }
+]);
+//Roll Number
+let rollNum = roll.number1;
+//Chencking Roll Number:
+if (rollNum == 12345) {
+    let marks = await inquirer.prompt([
+        {
+            name: "English",
+            type: "number",
+            message: chalk.bgYellowBright.red.italic("Please Enter Your English Number")
+        },
+        {
+            name: "Urdu",
+            type: "number",
+            message: chalk.bgRedBright.yellow.italic("Please Enter Your Urdu Number")
+        },
+        {
+            name: "Islamiat",
+            type: "number",
+            message: chalk.bgGreenBright.red.italic("Please Enter Your Islamiat Number")
+        },
+        {
+            name: "Math",
+            type: "number",
+            message: chalk.bgBlackBright.red.italic("Please Enter Your Math Number")
+        },
+        {
+            name: "Biology",
+            type: "number",
+            message: chalk.bgBlueBright.yellow.italic("Please Enter Your Biology Number")
+        },
+        {
+            name: "Chemistry",
+            type: "number",
+            message: chalk.bgRedBright.black.italic("Please Enter Your Chemistry Number")
+        },
+        {
+            name: "Physics",
+            type: "number",
+            message: chalk.bgCyanBright.black.italic("Please Enter Your physics Number")
+        }
+    ]);
+    //console.log(marks);
+    //Extract Marks
+    let english = marks.English;
+    let urdu = marks.Urdu;
+    let islamiat = marks.Islamiat;
+    let math = marks.Math;
+    let biology = marks.Biology;
+    let chemistry = marks.Chemistry;
+    let physics = marks.Physics;
+    //Percentage:
+    let totalMarks = 700;
+    let obtainMarks = english + urdu + islamiat + math + biology + chemistry + physics;
+    let percentage = ((obtainMarks / totalMarks) * 100);
+    //answer
+    console.log(chalk.yellow.underline.bold(`studentName: ${studentInformation.studentName}`));
+    console.log(chalk.blue.underline.bold(`FatherName: ${studentInformation.fatherName}`));
+    console.log(chalk.green.underline.bold("Marksheet for class Ten"));
+    console.log(chalk.black.underline.bold(`English Marks = ${english} out of 100`));
+    console.log(chalk.red.underline.bold(`Urdu Marks = ${urdu} out of 100`));
+    console.log(chalk.cyan.underline.bold(`Islamiat Marks = ${islamiat} out of 100`));
+    console.log(chalk.gray.underline.bold(`Math Marks = ${math} out of 100`));
+    console.log(chalk.red.underline.bold(`Biology Marks = ${biology} out of 100`));
+    console.log(chalk.magenta.underline.bold(`Chemistry Marks = ${chemistry} out of 100`));
+    console.log(chalk.black.underline.bold(`Chemistry Marks = ${physics} out of 100`));
+    console.log(chalk.bgYellowBright.red.bold(`Obtain Marks = ${obtainMarks}  Total Marks ${totalMarks}`));
+    console.log(chalk.bgMagentaBright.black.bold(`Percentage = ${percentage}%`));
+}
+else {
+    console.log(chalk.bgMagenta.bold("Invalid Roll Number"));
+}
